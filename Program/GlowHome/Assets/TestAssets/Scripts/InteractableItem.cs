f683270ce5968994f8c67661bb0f852790aff89c
B1 <bwlow39@gmail.com>
2023-08-22 20:53:15 +1000

    ADD Folder Structure, Materials and test interact script.

---
+++ Program/GlowHome/Assets/TestAssets/Scripts/InteractableItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+    public float duration;
+    private bool _state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
