9aa1c83e40282a8b966597e5e06b11c7018e02ca
B1 <bwlow39@gmail.com>
2023-08-25 14:27:26 +1000

    UPDATE Clean Interactable Item code
    Moved code from child class to parent class and added comments.

--- Program/GlowHome/Assets/TestAssets/Scripts/Items/Interactable.cs
+++ Program/GlowHome/Assets/TestAssets/Scripts/Items/Interactable.cs
@@ -12,15 +12,28 @@ public enum itemType
 
 public abstract class Interactable : MonoBehaviour
 {
-    public itemType type;
-    public string id;
+    public string id; //Unique identifier for game object
+    public itemType type; //Type of interactable item
     public int duration; //How long it takes to run
-    public List<Interactable> container = new List<Interactable>();
-
-    public abstract void Activate();
+    public List<Interactable> container = new List<Interactable>(); //Container for tied items
+    protected int _timeTaken; //How long appliance has been runnning
+    [SerializeField] protected bool _active;   //If device is on
+    [SerializeField] protected UnityEvent _on; //Called if item is on
+    [SerializeField] protected UnityEvent _off;//Called if item is off
     
-
-    private void OnMouseDown()
+    public abstract void Awake(); //Auto set variables for each
+    public abstract void Activate(); //The function called when user clicks on game object
+    public abstract void Reset(); //Object set back to base state
+    public abstract void Check(); //Called every minute of the clock
+    private void OnEnable()
+    {
+        TimeKeeper.onMinuteChanged += Check;
+    }
+    private void OnDisable()
+    {
+        TimeKeeper.onMinuteChanged -= Check;
+    }
+    private void OnMouseDown() //Call activate when object is clicked
     {
         if (container.Count == 0)
         {
