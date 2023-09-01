4d400ed6034522cdc8b3cfed7d9bc03295b7f81d
B1 <bwlow39@gmail.com>
2023-09-02 00:51:58 +1000

    UPDATE character nav 0.4
    Will no longer have vector 0 error and assigns isMoving to false.

--- Program/GlowHome/Assets/TestAssets/Scripts/Character.cs
+++ Program/GlowHome/Assets/TestAssets/Scripts/Character.cs
@@ -5,14 +5,17 @@ using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
-    public string name;
+    public string characterName;
     public GameObject room;
     public List<string> eventTimes;
     public List<GameObject> eventlocations;
+    public List<int> expectations;
 
     private bool _isMoving;
     private NavMeshAgent _agent;
     private Vector3 _destination;
+    private GameObject _destinationOBJ;
+    private int _satisfaction;
  
     private void OnEnable()
     {
@@ -32,6 +35,7 @@ public class Character : MonoBehaviour
     public void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _satisfaction = 0;
     }
     
     public void Check()
@@ -39,52 +43,41 @@ public class Character : MonoBehaviour
         if ( eventTimes.Contains(TimeKeeper.time))
         {
             _isMoving = true;
-            _destination = eventlocations[eventTimes.IndexOf(TimeKeeper.time)].transform.position;
+            _destinationOBJ = eventlocations[eventTimes.IndexOf(TimeKeeper.time)];
+            _destination = _destinationOBJ.transform.position;
         }
     }
 
     private void Update()
     {
+        //find the vector pointing from our position to the target
+		Vector3 _direction = (new Vector3 (_destination.x,_agent.transform.position.y,_destination.z) - transform.position).normalized;
+
+		if (_direction != Vector3.zero) {
+            //create the rotation we need to be in to look at the target
+		    Quaternion _lookRotation = Quaternion.LookRotation(_direction);
+            //rotate us over time according to speed until we are in the required rotation
+		    transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3);                    
+        }
+
+        Vector3 Islooking = (_destination - _agent.transform.position).normalized;
+        float dotProd = Vector3.Dot(Islooking, _agent.transform.forward);
+        
+        if(dotProd > 0.9 && _destinationOBJ.GetComponents<Component>().Length > 1 && Vector3.Distance(transform.position, _destination) < 2)
+        {
+            //if(_destinationOBJ.State() == (int)expectations[0])_satisfaction++;
+        }
+
         if (_isMoving)
         {
-            //find the vector pointing from our position to the target
-		    Vector3 _direction = (new Vector3 (_destination.x,_agent.transform.position.y,_destination.z) - transform.position).normalized;
-
-		    //create the rotation we need to be in to look at the target
-		    Quaternion _lookRotation = Quaternion.LookRotation(_direction);
-
-		    //rotate us over time according to speed until we are in the required rotation
-		    transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3);
-
-            Vector3 Islooking = (_destination - _agent.transform.position).normalized;
-            float dotProd = Vector3.Dot(Islooking, _agent.transform.forward);
-            if(dotProd > 0.9) {
+            if(dotProd > 0.9){
                 _agent.destination = _destination;
+            }
+            if(Vector3.Distance(transform.position, _destination) < 1 )
+            {
+                _isMoving = false;
+                print("STOP");
             }
         }
     }
-}
-
-public class SlerpToLookAt: MonoBehaviour
-{
-	//values that will be set in the Inspector
-	public Transform Target;
-	public float RotationSpeed;
-
-	//values for internal use
-	private Quaternion _lookRotation;
-	private Vector3 _direction;
-	
-	// Update is called once per frame
-	void Update()
-	{
-		//find the vector pointing from our position to the target
-		_direction = (Target.position - transform.position).normalized;
-
-		//create the rotation we need to be in to look at the target
-		_lookRotation = Quaternion.LookRotation(_direction);
-
-		//rotate us over time according to speed until we are in the required rotation
-		transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
-	}
 }
