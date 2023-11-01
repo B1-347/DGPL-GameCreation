1f7a6f68f854c1cd1b0c530920b32e8c7412a0b8
B1 <bwlow39@gmail.com>
2023-11-02 00:05:58 +1100

    ADD SUBMISSION
    .gitignore submissionfolder because of all the repeated files.

--- Program/GlowHome/Assets/TestAssets/Scripts/Room.cs
+++ Program/GlowHome/Assets/TestAssets/Scripts/Room.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public GameObject room;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
